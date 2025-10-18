52beeb94b5fe704375a5b6004bee518661a82f02
Alexis Lothoré <alexis.lothore@gmail.com>
2025-10-18 12:23:51 +0200

    package/neon-beat-backend: start tracking tagged versions
    The backend now has some tags, track the latest (0.1.3)
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

--- package/neon-beat-backend/neon-beat-backend.mk
+++ package/neon-beat-backend/neon-beat-backend.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-NEON_BEAT_BACKEND_VERSION = main
-NEON_BEAT_BACKEND_SITE = $(call github,neon-beat,neon-beat-back,$(NEON_BEAT_BACKEND_VERSION))
+NEON_BEAT_BACKEND_VERSION = 0.1.3
+NEON_BEAT_BACKEND_SITE = $(call github,neon-beat,neon-beat-back,v$(NEON_BEAT_BACKEND_VERSION))
 NEON_BEAT_BACKEND_LICENSE = GPL-3.0
 NEON_BEAT_BACKEND_LICENSE_FILES = COPYING
 NEON_BEAT_BACKEND_CARGO_BUILD_OPTS = --no-default-features --features couch-store
