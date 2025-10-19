14ef64333d16ba12183ea5b3f854f9e5986533a7
Alexis Lothoré <alexis.lothore@gmail.com>
2025-10-19 17:01:27 +0200

    package/neon-beat-backend: force feature flag on _any_ cargo command
    Setting the feature flags only on cargo build is not enough: when cargo
    install is executed, it will receive a different configuration, and then
    rebuild the software (leading to both DB backend being embedded). Make
    sure to get both build and install flags in sync to avoid this issue
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

--- package/neon-beat-backend/neon-beat-backend.mk
+++ package/neon-beat-backend/neon-beat-backend.mk
@@ -9,6 +9,7 @@ NEON_BEAT_BACKEND_SITE = $(call github,neon-beat,neon-beat-back,v$(NEON_BEAT_BAC
 NEON_BEAT_BACKEND_LICENSE = GPL-3.0
 NEON_BEAT_BACKEND_LICENSE_FILES = COPYING
 NEON_BEAT_BACKEND_CARGO_BUILD_OPTS = --no-default-features --features couch-store
+NEON_BEAT_BACKEND_CARGO_INSTALL_OPTS = --no-default-features --features couch-store
 
 define NEON_BEAT_BACKEND_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(NEON_BEAT_BACKEND_PKGDIR)/data/neon-beat-backend.service \
