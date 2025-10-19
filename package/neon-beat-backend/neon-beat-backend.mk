a620d998e3e8baf797fb97fb717a61403ba86175
Alexis Lothoré <alexis.lothore@gmail.com>
2025-10-19 23:24:48 +0200

    pacakge/neon-beat-back: update to newer version
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

--- package/neon-beat-backend/neon-beat-backend.mk
+++ package/neon-beat-backend/neon-beat-backend.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEON_BEAT_BACKEND_VERSION = 0.1.3
+NEON_BEAT_BACKEND_VERSION = 0.5.1
 NEON_BEAT_BACKEND_SITE = $(call github,neon-beat,neon-beat-back,v$(NEON_BEAT_BACKEND_VERSION))
 NEON_BEAT_BACKEND_LICENSE = GPL-3.0
 NEON_BEAT_BACKEND_LICENSE_FILES = COPYING
