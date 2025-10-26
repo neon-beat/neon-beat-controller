7393d3fd71773e91bd042ca4cd30d3114c3ff608
Alexis Lothoré <alexis.lothore@gmail.com>
2025-10-26 23:20:39 +0100

    scripts: add a fast-redeploy helper
    Add a small script which:
    - cleans dl dir from any Neon beat artifact
    - dirclean all Neon Beat components
    - rebuild Neon Beat components
    - deploy Neon Beat components onto the NBC
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

---
+++ scripts/rebuild-and-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# This script assumes that
+# - the NBC is up and running
+# - you have deployed your SSH public key onto it
+# - you can ping nbc.local
+# - you have set an entry for the NBC in your ssh_config to access it with
+# a bare "ssh nbc"
+
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+TOP_DIR=${SCRIPT_DIR}/..
+
+rm -rf ${TOP_DIR}/dl/neon-beat-*
+make -C ${TOP_DIR} neon-beat-{{game,admin}-frontend,backend}-dirclean all
+make -C ${TOP_DIR} neon-beat-{{game,admin}-frontend,backend}
+ssh nbc "systemctl stop neon-beat-backend"
+scp -O $(TOP_DIR)/output/target/usr/bin/neon-beat-back nbc:/usr/bin
+scp -rO $(TOP_DIR)/output/target/srv/www nbc:/srv
+ssh nbc "systemctl start neon-beat-backend"
