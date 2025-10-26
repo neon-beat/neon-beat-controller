25153d7864eb11531f7e21aae0b5f46d314ff333
Alexis Lothoré <alexis.lothore@gmail.com>
2025-10-26 23:19:23 +0100

    package/neon-beat-backend: build only the main binary
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

--- package/neon-beat-backend/neon-beat-backend.mk
+++ package/neon-beat-backend/neon-beat-backend.mk
@@ -8,8 +8,14 @@ NEON_BEAT_BACKEND_VERSION = 0.7.0
 NEON_BEAT_BACKEND_SITE = $(call github,neon-beat,neon-beat-back,v$(NEON_BEAT_BACKEND_VERSION))
 NEON_BEAT_BACKEND_LICENSE = GPL-3.0
 NEON_BEAT_BACKEND_LICENSE_FILES = COPYING
-NEON_BEAT_BACKEND_CARGO_BUILD_OPTS = --no-default-features --features couch-store
-NEON_BEAT_BACKEND_CARGO_INSTALL_OPTS = --no-default-features --features couch-store
+NEON_BEAT_BACKEND_CARGO_BUILD_OPTS = \
+				     --bin neon-beat-back \
+				     --no-default-features \
+				     --features couch-store
+NEON_BEAT_BACKEND_CARGO_INSTALL_OPTS = \
+				       --bin neon-beat-back \
+				       --no-default-features \
+				       --features couch-store
 
 define NEON_BEAT_BACKEND_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(NEON_BEAT_BACKEND_PKGDIR)/data/neon-beat-backend.service \
