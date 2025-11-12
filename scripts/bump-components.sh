46355c3795a02edf89ef37aa1477ce7cd5c09355
Alexis Lothoré <alexis.lothore@gmail.com>
2025-11-12 22:44:12 +0100

    scripts: update the components bump tool
    The helper allowing to automatically bump component versions is able to
    update both package version and SHA1, but it currently fails to compute
    the correct SHA1 for the backend. The issue is due to the fact that when
    buildroot downloads a cargo based project, it will "vendor" the
    dependencies into the downloaded project (ie: it will run cargo vendor
    --locked VENDOR in the project directory), and then retar the project
    and compute its hash. Thanks to this, the dependencies are part of the
    hash.
    
    Follow the same strategy in the bump script. This is not operational
    yet: the computed hash is still not the exact same one as computed by
    buildroot, however we are getting closer: some manual inspection allowed
    to validate that the directory content, after manual vendoring, is
    exactly _the same_ as the buildroot one. So the failing step is either
    in:
    - the tar command
    - the gzip command
    - the hash computation
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@gmail.com>

--- scripts/bump-components.sh
+++ scripts/bump-components.sh
@@ -7,11 +7,43 @@ set -e
 
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 TOP_DIR=${SCRIPT_DIR}/..
+CARGO=${TOP_DIR}/output/host/bin/cargo
 
 declare -A COMPONENTS
 COMPONENTS["neon-beat-backend"]="neon-beat-back"
 COMPONENTS["neon-beat-game-frontend"]="neon-beat-game-front"
 COMPONENTS["neon-beat-admin-frontend"]="neon-beat-admin-front"
+
+if [ ! -f ${CARGO} ]
+then
+	echo "You need cargo-host to be built and installed in buildroot host dir"
+	exit 1
+fi
+
+# For cargo packages, buildroot includes the vendored dependencies in the
+# SHA256 sum, so to get the updated sum, we need to:
+# - uncompress the downloaded archive
+# - vendor the deps inside it (with host-cargo from BR)
+# - re-tar/re-compress it
+compute_vendored_component_sha256() {
+	ARCHIVE="${1}.tar.gz"
+	WORKDIR="$(echo ${2}|cut -d '.' -f 1)-${3}"
+	tar xzvf ${ARCHIVE} -C /tmp > /dev/null
+	pushd "/tmp/${WORKDIR}" > /dev/null
+	mkdir .cargo
+	${CARGO} vendor --locked VENDOR > .cargo/config.toml
+	find . -not -type d > /tmp/files.list
+	LC_ALL=C sort < /tmp/files.list > /tmp/files_sorted.list
+	tar -cf - --transform="s#^\./#${WORKDIR}/#S" \
+		--numeric-owner --owner=0 --group=0 \
+		--pax-option=delete=atime,delete=ctime,delete=mtime,exthdr.name=%d/PaxHeaders/%f \
+		--mode='go=u,go-w' --format=posix -T /tmp/t-sorted.list > \
+		/tmp/${1}.tar > /dev/null
+	popd > /dev/null
+	gzip -6 -n < "/tmp/${1}.tar" > "/tmp/${ARCHIVE}" 2>/dev/null
+
+	echo $(sha256sum "/tmp/${ARCHIVE}"|cut -d ' ' -f 1)
+}
 
 for c in ${!COMPONENTS[@]}
 do
@@ -38,7 +70,13 @@ do
 			echo "Upgrade component ${c} from ${current_version} to ${version}"
 			archive="${c}.tar.gz"
 			curl -so ${archive} https://codeload.github.com/neon-beat/${COMPONENTS[${c}]}/tar.gz/refs/tags/${raw_version}
-			new_sha256=$(sha256sum ${archive}|cut -d ' ' -f 1)
+			if [ ${c} == "neon-beat-backend" ]
+			then
+				new_sha256=$(compute_vendored_component_sha256 ${c} ${COMPONENTS[${c}]} ${version})
+			else
+				new_sha256=$(sha256sum ${archive}|cut -d ' ' -f 1)
+			fi
+			echo "New sha256 sum: ${new_sha256}"
 			sed -i "s/^\(${version_field} *= *\).*/\1${version}/" ${mk_file}
 			sed -i "s/sha256  [0-9a-f].*  ${c}-${current_version}\(.*\)$/sha256  ${new_sha256}  ${c}-${version}\1/" ${hash_file}
 			rm -rf ${archive}
